3d47cc90479b137661bb67db173537d7fd845185
Vladas Zakrevskis <146100@gmail.com>
2026-01-08 16:53:18 +0200

    random behind feature

--- reflected/src/lib.rs
+++ reflected/src/lib.rs
@@ -1,5 +1,6 @@
 mod field;
 mod field_type;
+#[cfg(feature = "random")]
 mod random;
 mod reflected;
 mod reflected_eq;
@@ -8,6 +9,7 @@ mod to_reflected_val;
 
 pub use field::Field;
 pub use field_type::{OptionalType, Type};
+#[cfg(feature = "random")]
 pub use random::RandomReflected;
 pub use reflected::Reflected;
 pub use reflected_eq::ReflectedEq;
--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -54,7 +54,7 @@ mod test {
     use std::str::FromStr;
 
     use chrono::{Duration, NaiveDateTime, TimeDelta, Utc};
-    use reflected::{RandomReflected, Reflected, ReflectedEq};
+    use reflected::{Reflected, ReflectedEq};
     use rust_decimal::Decimal;
 
     use crate::{User, sercli};
@@ -325,12 +325,5 @@ mod test {
             ),
             "5:25"
         );
-    }
-
-    #[test]
-    fn random() {
-        let user = User::random();
-
-        dbg!(&user);
     }
 }
